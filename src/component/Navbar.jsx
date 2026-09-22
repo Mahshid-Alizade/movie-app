1cceac29939fea58f5bd41b74a239c1ccc414ad5
Mahshid-Alizade <mahshidalz7878@gmail.com>
2026-09-22 12:31:38 +0200

    css with tailwind

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,39 +1,61 @@
 import React from "react";
+import { User, Search, Gem, Bell } from "lucide-react";
+// import "../css/Navbar.css";
 
 export default function Navbar() {
+  const navLinks = [
+    "Films",
+    "Series",
+    "Movie Buffs",
+    "Lists",
+    "Artist",
+    "Blog",
+    "Sections",
+  ];
+
+  const actions = [
+    { name: "Search", icon: Search },
+    { name: "Subscribe", icon: Gem },
+    { name: "Notifications", icon: Bell },
+    { name: "Log in", icon: User },
+  ];
+
   return (
-    <div className="nav-container">
-      <a className="nav-logo">LOGO</a>
-      <nav className="nav-links">
-        <a href="#" className="nav-link">
-          Films
-        </a>
-        <a href="#" className="nav-link">
-          Series
-        </a>
-        <a href="#" className="nav-link">
-          Movie Buffs
-        </a>
-        <a href="#" className="nav-link">
-          Lists
-        </a>
-        <a href="#" className="nav-link">
-          Artist
-        </a>
-        <a href="#" className="nav-link">
-          Blog
-        </a>
-        <a href="#" className="nav-link">
-          Sections
-        </a>
-      </nav>
-      <div className="nav-actions">
-        <button className="nav-action nav-action--notification"></button>
-        <button className="nav-action nav-action--subscription">
-          Subscribe
-        </button>
-        <button className="nav-action nav-action--search"></button>
-        <button className="nav-action nav-action--login">Log in</button>
+    <div className="flex justify-between items-center min-h-16 px-4 bg-black">
+      <div className="flex gap-8">
+        <a className="text-lg font-bold text-white tracking-widest">LOGO</a>
+        <nav className="flex gap-2 text-sm items-center">
+          {navLinks.map((link) => (
+            <a
+              key={link}
+              href="#"
+              className="rounded px-[10px] py-[5px] text-white transition-all duration-300 hover:bg-[rgba(255,196,0,0.833)] hover:text-black"
+            >
+              {link}
+            </a>
+          ))}
+        </nav>
+      </div>
+
+      <div className="flex gap-2">
+        {actions.map((action) => {
+          return (
+            <button
+              key={action.name}
+              href="#"
+              className="group rounded border-2 bg-[#00000046] px-[10px] py-[5px] text-white transition-all duration-300 hover:border-[rgba(255,196,0,0.721)]"
+            >
+              {action.icon ? (
+                <action.icon
+                  size={18}
+                  className="transition-all duration-300 group-hover:text-[rgba(255,196,0,0.833)]"
+                />
+              ) : (
+                action.name
+              )}
+            </button>
+          );
+        })}
       </div>
     </div>
   );
